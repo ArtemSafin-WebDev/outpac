079df3ec6cdbbc6604ed63a9a5cf4e9f995015e8
Artem Safin <artemsafin.webdev@gmail.com>
2021-02-09 23:37:45 +0300

    Сделал нативные селекты в корзине

--- src/js/customSelects.js
+++ src/js/customSelects.js
@@ -1,6 +1,10 @@
 import Choices from 'choices.js';
+import { MOBILE_WIDTH } from './constants';
 
 export default function customSelects() {
+    if (window.matchMedia(`(max-width: ${MOBILE_WIDTH}px)`).matches) {
+        return;
+    }
     const customSelects = Array.from(document.querySelectorAll('.js-custom-select'));
 
     customSelects.forEach(select => {
