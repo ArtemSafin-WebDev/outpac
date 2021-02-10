c72cba827a7ddf86978c2fe5d2c00ff89a217aa1
Artem Safin <artemsafin.webdev@gmail.com>
2021-02-11 02:48:46 +0300

    Фиксы по адаптиву

--- src/js/stickyProduct.js
+++ src/js/stickyProduct.js
@@ -5,7 +5,17 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function stickyProduct() {
     ScrollTrigger.matchMedia({
-        '(min-width: 641px)': () => {
+        '(min-width: 641px) and (max-width: 1024px)': () => {
+            ScrollTrigger.create({
+                trigger: '.js-sticky-product-sidebar',
+                start: 'top top',
+                endTrigger: '.js-sticky-product-images',
+                end: 'bottom bottom',
+                pin: true,
+                pinSpacing: true
+            });
+        },
+        '(min-width: 1025px)': () => {
             ScrollTrigger.create({
                 trigger: '.js-sticky-product-sidebar',
                 start: 'bottom bottom',
